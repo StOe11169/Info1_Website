2909678e5e7ef1e39bb8dd56c2b79b752adb9b58
Stefan Oelbracht <stefan.oelbracht@studmail.w-hs.de>
2023-05-21 14:48:54 +0200

    Merged fluidSim_StOe.html and simulatorEuler.php into one file.

--- simulatorEuler.php
+++ simulatorEuler.php
@@ -6,6 +6,7 @@
     <title>Simulator</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="styles/sidebar.css">
+    <link href="styles/simulator.css" rel="stylesheet">
 </head>
 <body>
 
@@ -22,7 +23,70 @@
             <li class="breadcrumb-item active" aria-current="page">Euler Simulator</li>
         </ol>
     </nav>
+
+    <button class="button" onclick="setupScene(1)">Wind Tunnel</button>
+    <button class="button" onclick="setupScene(3)">Hires Tunnel</button>
+    <button class="button" onclick="setupScene(0)">Tank</button>
+    <button class="button" onclick="setupScene(2)">Paint</button>
+    <input id="densitySlider" max="10000" min="500" type="range" value="1000">
+    <input id="viscositySlider" max="0.0001" min="0.00000000000001" type="range" value="0.0000001">
+    <input id="streamButton" onclick="scene.showStreamlines = !scene.showStreamlines" type="checkbox">Streamlines
+    <input id="velocityButton" onclick="scene.showVelocities = !scene.showVelocities" type="checkbox">Velocities
+    <input id="pressureButton" name="field" onclick="scene.showPressure = !scene.showPressure;" type="checkbox"> Pressure
+    <input checked id="smokeButton" name="field" onclick="scene.showSmoke = !scene.showSmoke;" type="checkbox">Smoke
+    <input checked id="overrelaxButton" onclick="scene.overRelaxation = scene.overRelaxation === 1.0 ? 1.9 : 1.0"
+           type="checkbox">Overrelax>
+
+    <canvas id="myCanvas" style="border:2px solid"></canvas>
+    <script src="scripts/FluidSim.js"></script>
+    <script src="scripts/visualisation.js"></script>
+    <script>
+
+        //TODO Sliding obstacle over source "blocks" it, moving obstacle into source does not.
+        //TODO Change all finite difference methods to central difference method
+        //TODO Maybe make difference methods modular
+        //TODO Decouple render speed from canvas size
+        //TODO Add method to control and compare different numerical methods
+        //TODO Let canvas size vary but sim size stays same, only visually stretched
+        //TODO 3D with WebGl or p5.js
+        //TODO Update and expand GUI
+        //TODO decouple simulation speed from rendering speed
+        //TODO Add dimensions to all physical units
+        //TODO Make number of cells independent of sim cell size
+        //TODO Check if dynamic viscosity and kinematic viscosity have not been falsly used.
+        //! dynamic visocsity = kinematic viscosity * density. my = ny * rho
+
+        setupScene(1);
+        runSimulationLoop();
+
+    </script>
+
     <div class="d-flex" id="wrapper">
+
+        <canvas id="myCanvas" style="border:2px solid"></canvas>
+        <script src="scripts/FluidSim.js"></script>
+        <script src="scripts/visualisation.js"></script>
+        <script>
+
+            //TODO Sliding obstacle over source "blocks" it, moving obstacle into source does not.
+            //TODO Change all finite difference methods to central difference method
+            //TODO Maybe make difference methods modular
+            //TODO Decouple render speed from canvas size
+            //TODO Add method to control and compare different numerical methods
+            //TODO Let canvas size vary but sim size stays same, only visually stretched
+            //TODO 3D with WebGl or p5.js
+            //TODO Update and expand GUI
+            //TODO decouple simulation speed from rendering speed
+            //TODO Add dimensions to all physical units
+            //TODO Make number of cells independent of sim cell size
+            //TODO Check if dynamic viscosity and kinematic viscosity have not been falsly used.
+            //! dynamic visocsity = kinematic viscosity * density. my = ny * rho
+
+            setupScene(1);
+            runSimulationLoop();
+
+        </script>
+        <!--TODO Sidebar pushed out of view to the left by canvas -->
 <!-- Sidebar -->
 <div class="bg-light border-right vh-100" id="sidebar-wrapper">
     <div class="sidebar-heading">Start Bootstrap </div>
